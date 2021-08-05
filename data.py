a85588aab5222325458dd288f27d8f495b13eb84
sagrawal1234 <70326310+sagrawal1234@users.noreply.github.com>
2021-08-05 17:06:31 +0530

    Add files via upload

--- data.py
+++ data.py
@@ -28,6 +28,8 @@ for i in range(1,len(temp_list)):
     distance.append(temp_list[i][5])
     mass.append(temp_list[i][6])
     star_names.append(temp_list[i][7])
+    lum.append(temp_list[i][7]) 
+
 
 df2 = pd.DataFrame(list(zip(star_names,distance,mass,radius,lum)),coloumns = ['star_names','distance','mass','radius','luminosity'])
 print(df2)
